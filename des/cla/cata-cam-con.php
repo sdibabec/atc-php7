c59a7fd0abe5fc52f36b217a3ed6d3a0f2ad2178
Mario Ernesto Basurto Medrano <mario@d80.mx>
2021-03-22 16:15:45 -0600

    camionetas

---
+++ des/cla/cata-cam-con.php
@@ -0,0 +1,120 @@
+
+
+
+
+
+<?php
+
+
+
+
+
+
+session_start();
+
+$bAll = $_SESSION['bAll'];
+$bDelete = $_SESSION['bDelete'];
+
+$errores = array();
+
+
+
+/*Preparacion de variables*/
+
+$codigo = $data->eCodAccion ? $data->eCodAccion : $data->eAccion;
+$accion = $data->tCodAccion ? $data->tCodAccion : $data->tAccion;
+
+$eCodCamioneta = $data->eCodCamioneta ? $data->eCodCamioneta : false;
+$eCodCliente = $data->eCodCliente ? $data->eCodCliente : false;
+$eCodEstatus = $data->eCodEstatus ? $data->eCodEstatus : false;
+$fhFechaInicio = $data->fhFechaConsulta1 ? explode("/",$data->fhFechaConsulta1) : false;
+$fhFechaFin = $data->fhFechaConsulta2 ? explode("/",$data->fhFechaConsulta2) : $fhFechaInicio;
+
+$fhFecha1 = $fhFechaInicio[2].'-'.$fhFechaInicio[1].'-'.$fhFechaInicio[0];
+$fhFecha2 = $data->fhFechaConsulta2 ? $fhFechaFin[2].'-'.$fhFechaFin[1].'-'.$fhFechaFin[0] : $fhFecha1;
+
+$eLimit = $data->eMaxRegistros;
+$bOrden = $data->rOrden;
+$rdOrden = $data->rdOrden ? $data->rdOrden : 'eCodCamioneta';
+
+switch($accion)
+{
+    case 'D':
+        $insert = "UPDATE BitEventos SET eCodEstatus = 4 WHERE eCodCamioneta = ".$codigo;
+        break;
+    case 'F':
+        $insert = "UPDATE BitEventos SET eCodEstatus = 8 WHERE eCodCamioneta = ".$codigo;
+        break;
+    case 'C':
+        $tHTML =  '<table class="table table-hover" width="100%">'.
+        '<thead>'.
+        '<tr>'.
+        '<th class="text-right">Codigo</th>'.
+        '<th class="text-right">E</th>'.
+        '<th class="text-right">Nombre</th>'.
+		'</tr>'.
+        '</thead>'.
+        '<tbody>';
+        /* hacemos select */
+        $select = "SELECT * FROM (SELECT cc.*, ce.tIcono estatus FROM CatCamionetas cc INNER JOIN CatEstatus ce ON ce.tCodEstatus=cc.tCodEstatus ORDER BY cc.eCodCamioneta ASC ".
+		($bAll ? "" : " AND cc.eCodUsuario = ".$_SESSION['sessionAdmin']['eCodUsuario']).
+        //($bAll ? "" : " AND be.eCodEstatus<>4").
+        ($eCodCamioneta ? " AND be.eCodCamioneta = $eCodCamioneta" : "").
+        ($eCodCliente ? " AND be.eCodCliente = $eCodCliente" : "").
+        ($eCodEstatus ? " AND ce.eCodEstatus = $eCodEstatus" : "").
+        ($data->fhFechaConsulta1 ? " AND DATE(be.fhFechaEvento) BETWEEN  '$fhFecha1' AND '$fhFecha2'" : "").
+        " LIMIT 0, $eLimit ".
+		")N0 ORDER BY $rdOrden $bOrden";
+		
+        $rsConsulta = mysqli_query($conexion,$select);
+        while($rConsulta=mysqli_fetch_array($rsConsulta)){
+         /* validamos si está cargado */
+            $bCargado = (mysqli_num_rows(mysqli_query($conexion,"SELECT * FROM SisRegistrosCargas WHERE eCodCamioneta = ".$rConsulta{'eCodCamioneta'}))) ? true : false;
+            
+            $arrEstados = array();
+            $arrEstados[0] = true;
+            $arrEstados[1] = ($rConsulta{'eCodEstatus'}==1 || $rConsulta{'eCodEstatus'}==2) ? true : false;
+            $arrEstados[2] = true;
+            $arrEstados[3] = ($rConsulta{'eCodEstatus'}==1 || $rConsulta{'eCodEstatus'}==2) ? true : false;
+            $arrEstados[4] = ($rConsulta{'eCodEstatus'}==1 || $rConsulta{'eCodEstatus'}==2) ? true : false;
+            $arrEstados[5] = ($rConsulta{'eCodEstatus'}==1 || $rConsulta{'eCodEstatus'}==2) ? true : false;
+            
+            //imprimimos
+       $tHTML .=    '<tr>'.
+                    '<td>'.$clNav->menuEmergenteJSON($rConsulta{'eCodCamioneta'},'cata-cam-con',$arrEstados).'</td>'.
+                    '<td align="center"><i class="'.$rConsulta{'tIcono'}.'"></i></td>'.
+			        '<td>'.utf8_decode($rConsulta{'tNombre'}).'</td>'.
+                    '</tr>';
+            //imprimimos
+        }
+        /* hacemos select */
+        $tHTML .= '</tbody>'.
+            '</table>';
+        break;
+}
+        
+if($accion=="D" || $accion=="F")
+{
+    $rs = mysqli_query($conexion,$insert);
+
+    if(!$rs)
+    {
+        $errores[] = 'Error al efectuar la operacion '.mysqli_error($conexion);
+    }
+
+    if(!sizeof($errores))
+    {
+        $tDescripcion = "Se ha ".(($accion=="D") ? 'Eliminado' : 'Finalizado')." la renta código ".sprintf("%07d",$codigo);
+        $tDescripcion = "'".utf8_encode($tDescripcion)."'";
+        $fecha = "'".date('Y-m-d H:i:s')."'";
+        $eCodUsuario = $_SESSION['sessionAdmin']['eCodUsuario'];
+        mysqli_query($conexion,"INSERT INTO SisLogs (eCodUsuario, fhFecha, tDescripcion) VALUES ($eCodUsuario, $fecha, $tDescripcion)");
+    }
+    
+    echo json_encode(array("exito"=>((!sizeof($errores)) ? 1 : 0), 'errores'=>$errores));
+    
+}
+
+echo json_encode(array("exito"=>((!sizeof($errores)) ? 1 : 0), 'errores'=>$errores,'registros'=>(int)mysqli_num_rows($rsConsulta),"consulta"=>$tHTML));
+
+?>
--- des/mod/veh/cata-cam-con.php
+++ des/mod/veh/cata-cam-con.php
@@ -7,49 +7,80 @@
 session_start();
 $bAll = $_SESSION['bAll'];
 $bDelete = $_SESSION['bDelete'];
-
+$select = " SELECT DISTINCT eCodEstatus,tNombre tEstatus FROM CatEstatus WHERE tCodEstatus IN (SELECT DISTINCT tCodEstatus FROM CatCamionetas)";
+$rsEstatus = mysqli_query($conexion,$select);
 ?>
 <script>
-function detalles(eCodCliente)
-    {
-        window.location="?tCodSeccion=cata-cli-det&eCodCliente="+eCodCliente;
-    }
-function exportar()
-    {
-        window.location="gene-cli-xls.php";
-    }
-</script>
-<div class="row">
-                            <div class="col-lg-12">
+    $(document).ready(function() {
+             filtrar();
+          });
+</script>                       
+<div class="col-lg-12">
+                            <div class="row">
                                 
                                 
-                                    <table class="display" id="cliTable" width="100%">
-                                        <thead>
-                                            
-                                            <tr>
-                                                <th>C&oacute;digo</th>
-												<th>E</th>
-												<th>Nombre</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-											<?
-											$select = "	SELECT cc.*, ce.tIcono estatus FROM CatCamionetas cc INNER JOIN CatEstatus ce ON ce.tCodEstatus=cc.tCodEstatus ORDER BY cc.eCodCamioneta ASC ";
-											$rsPublicaciones = mysql_query($select);
-											while($rPublicacion = mysql_fetch_array($rsPublicaciones))
-											{
-                                                
-												?>
-											<tr>
-                                                <td><? menuEmergente($rPublicacion{'eCodCamioneta'}); ?></td>
-                                                <td><i class="<?=$rPublicacion{'estatus'}?>"></i></td>
-                                                <td><?=utf8_decode($rPublicacion{'tNombre'})?></td>
-                                            </tr>
-											<?
-											}
-											?>
-                                        </tbody>
-                                    </table>
+                                <!--filtros-->
+                                <div class="col-md-12">
+                                    <div id="eRegistros" style="font-size:16px; font-weight:bold;"></div>
+                                    
+                                </div>
+                                
+                                <div class="clearfix"></div>
+                                
+                                <div id="divFiltros" style="display:none;" class="card card-body table-responsive">
+                                <form id="Datos" name="Datos">
+                                    <input type="hidden" name="tAccion" id="tAccion" value="">
+                                    <input type="hidden" name="eAccion" id="eAccion" value="">
+                                    <table width="100%" cellpadding="10" class="table">
+                                    <tr hidden>
+                                        <td width="25%"></td>
+                                        <td width="25%"></td>
+                                        <td width="25%"></td>
+                                        <td width="25%"></td>
+                                    </tr>
+<tr>
+    <td><label><input type="radio" name="rdOrden" value="eCodCamioneta" checked="checked"> C&oacute;digo</label></td>
+    <td>
+        <input type="text" class="form-control" name="eCodCamioneta" id="eCodCamioneta">
+    </td>
+    <td><label><input type="radio" name="rdOrden" value="eCodEstatus"> Estatus</label></td>
+    <td>
+        <select id="eCodEstatus" name="eCodEstatus" class="form-control">
+        <option value="">Seleccione...</option>
+        <? while($rEstatus = mysqli_fetch_array($rsEstatus)) { ?>
+            <option value="<?=$rEstatus{'eCodEstatus'};?>"><?=$rEstatus{'tEstatus'};?></option>
+        <? } ?>
+        </select>
+    </td>
+</tr>
+<tr>
+    <td>Mostrar</td>
+    <td>
+        <select id="eMaxRegistros" name="eMaxRegistros"  class="form-control">
+        <? while($rRegistro = mysqli_fetch_array($rsMaximos)) { ?>
+            <option value="<?=$rRegistro{'eRegistros'};?>"><?=$rRegistro{'eRegistros'};?> registros</option>
+        <? } ?>
+        </select>
+    </td>
+    <td>Orden</td>
+    <td>
+        <select id="rOrden" name="rOrden"  class="form-control">
+        <option value="DESC">Descendente</option>
+        <option value="ASC">Ascendente</option>
+        </select>
+    </td>
+</tr>
+</table>
+                                    </form>
+                                </div>
+                                
+                                <div class="clearfix" style="padding:10px;"><img src="/images/separador.jpg" class="img-responsive" style="width:100%;"></div>
+                                <!--filtros-->
+                                
+                                <!--tabla-->
+                                <div class="card card-body table-responsive table-responsive-data2" id="divXHR" style="min-height:350px;">
+                                </div>
+                                <!--tabla-->
                                 
                             </div>
                         </div>

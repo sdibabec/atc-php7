fdfec6a033ecb522a2bf10c173c57deb7e519509
Mario Ernesto Basurto Medrano <mario@d80.mx>
2021-03-22 12:56:02 -0600

    Update aplicacion.js

--- assets/js/aplicacion.js
+++ assets/js/aplicacion.js
@@ -339,47 +339,8 @@ function deleteRow(rowid,tabla)  {
                         agregarFilaExtra(0);
                     }
             }
-        if(tabla=="clientes")
-            {
-               var x = document.getElementById("clientes").rows.length;
-                if(x<2)
-                    {
-                        agregarFilaCliente(0);
-                    }
-            }
-        if(tabla=="supervisores")
-            {
-               var x = document.getElementById("supervisores").rows.length;
-                if(x<2)
-                    {
-                        agregarFilaSupervisor(0);
-                    }
-            }
-        if(tabla=="promotores")
-            {
-               var x = document.getElementById("promotores").rows.length;
-                if(x<2)
-                    {
-                        agregarFilaPromotor(0,0);
-                    }
-            }
-        if(tabla=="productos")
-            {
-               var x = document.getElementById("productos").rows.length;
-                if(x<2)
-                    {
-                        agregarFilaProducto(0);
-                    }
-            }
-        if(tabla=="presentaciones")
-            {
-               var x = document.getElementById("presentaciones").rows.length;
-                if(x<2)
-                    {
-                        agregarFilaPresentacion(0,0);
-                    }
-            }
-       
+        
+        calcular();
 }
 
 
@@ -428,172 +389,122 @@ function agregarFilaExtra(indice)
         
     }
 
-//tienda
+//inventario
 
-function validarTienda(indice)
+function calcularTotalInventario(indice)
+{
+   var eCodInventario    =   document.getElementById('inventario'+indice+'-eCodInventario'),
+            ePiezas        =   document.getElementById('inventario'+indice+'-ePiezas'),
+            dImporte = document.getElementById('inventario'+indice+'-dImporte'),
+            dMonto = document.getElementById('inventario'+indice+'-dMonto');
+        
+        if(eCodInventario.value && ePiezas.value)
+            {
+                dMonto.value = parseInt(ePiezas.value)*parseInt(dImporte.value);   
+            } 
+}
+
+function validarInventario(indice)
     {
-        var eCodTienda    =   document.getElementById('eCodTienda'+indice),
+        var eCodInventario    =   document.getElementById('inventario'+indice+'-eCodInventario'),
+            ePiezas        =   document.getElementById('inventario'+indice+'-ePiezas'),
+            dImporte = document.getElementById('inventario'+indice+'-dImporte'),
+            dMonto = document.getElementById('inventario'+indice+'-dMonto'),
             nIndice         =   parseInt(indice)+1;
         
-        if(eCodTienda.value)
+        if(eCodInventario.value && ePiezas.value)
             {
-                agregarFilaTienda(nIndice);    
+                dMonto.value = parseInt(ePiezas.value)*parseInt(dImporte.value);
+                agregarFilaInventarioCotizacion(nIndice);    
             }
     }
     
-function agregarFilaTienda(indice)
+function agregarFilaInventarioCotizacion(indice)
     {
-        var x = document.getElementById("tiendas").rows.length;
+        var x = document.getElementById("invs").rows.length;
         
         
-        var eCodTienda = document.getElementById('eCodTienda'+indice);
-        if(eCodTienda)
+        var tExtra = document.getElementById('inventario'+indice+'-eCodInventario');
+        if(tExtra)
             {}
         else
         {
            
-    var table = document.getElementById("tiendas");
+    var table = document.getElementById("invs");
     var row = table.insertRow(x);
-    row.id="tie"+(indice);
-    row.innerHTML = '<td><i class="far fa-trash-alt" onclick="deleteRow(\'tie'+indice+'\',\'tiendas\')"></i></td>';
-    row.innerHTML += '<td><input type="hidden" id="eCodTienda'+indice+'" name="tiendas['+indice+'][eCodTienda]"><input type="text" class="form-control" id="tTienda'+indice+'" name="tiendas['+indice+'][tTienda]" onkeyup="agregarTienda('+indice+')" onkeypress="agregarTienda('+indice+')" onblur="validarTienda('+indice+')"></td>';
+    row.id="inv"+(indice);
+    row.innerHTML = '<td><i class="far fa-trash-alt" onclick="deleteRow(\'inv'+indice+'\',\'invs\')"></i></td>';
+    row.innerHTML += '<td><input type="checkbox" id="inventario'+indice+'-bSuma" name="inventario['+indice+'][bSuma]" value="1" onclick="calcular();"></td><td><input type="hidden" id="inventario'+indice+'-eCodInventario" name="inventario['+indice+'][eCodInventario]"><input type="text" class="form-control" id="tInventario'+indice+'" name="tInventario'+indice+'" onkeyup="agregarInventario('+indice+')" onkeypress="agregarInventario('+indice+')" onblur="validarInventario('+indice+')"></td>';
+    row.innerHTML += '<td><input type="hidden" id="inventario'+indice+'-eMaxPiezas"><input type="hidden" id="inventario'+indice+'-dImporte" name="inventario['+indice+'][dImporte]"><input type="text" class="form-control" id="inventario'+indice+'-ePiezas" name="inventario['+indice+'][ePiezas]" onkeyup="validarPiezas(\'inventario'+indice+'\'); validarInventario('+indice+')" onblur="validarInventario('+indice+')"></td><td><input type="text"  id="inventario'+indice+'-dMonto" name="inventario['+indice+'][dMonto]" readonly></td>';
         }
+        
+    calcular();
         
     }
 
-//supervisor
-
-function validarSupervisor(indice)
+//paquetes
+function calcularTotalPaquete(indice)
     {
-        var eCodSupervisor    =   document.getElementById('eCodSupervisor'+indice),
+        var eCodServicio    =   document.getElementById('paquete'+indice+'-eCodServicio'),
+            ePiezas        =   document.getElementById('paquete'+indice+'-ePiezas'),
+            dImporte = document.getElementById('paquete'+indice+'-dImporte'),
+            dMonto = document.getElementById('paquete'+indice+'-dMonto');
+        
+        if(eCodServicio.value && ePiezas.value)
+            {
+                dMonto.value = parseInt(ePiezas.value)*parseInt(dImporte.value);
+            }
+    }
+
+function calcularFilaPaquete(indice)
+    {
+        var eHorasExtra = document.getElementById('paquete'+indice+'-eHorasExtra'),
+            dHoraExtra = document.getElementById('paquete'+indice+'-dHoraExtra'),
+            dImporte = document.getElementById('paquete'+indice+'-dImporte'),
+            ePiezas = document.getElementById('paquete'+indice+'-ePiezas'),
+            dMonto = document.getElementById('paquete'+indice+'-dMonto');
+        
+        if(eHorasExtra.value && eHorasExtra.value>0)
+            {
+                dMonto.value = (parseInt(ePiezas.value)*parseInt(dImporte.value)) + (parseInt(eHorasExtra)*parseInt(dHoraExtra));
+            }
+    }
+
+function validarPaquete(indice)
+    {
+        var eCodServicio    =   document.getElementById('paquete'+indice+'-eCodServicio'),
+            ePiezas        =   document.getElementById('paquete'+indice+'-ePiezas'),
+            dImporte = document.getElementById('paquete'+indice+'-dImporte'),
+            dMonto = document.getElementById('paquete'+indice+'-dMonto'),
             nIndice         =   parseInt(indice)+1;
         
-        if(eCodSupervisor.value)
+        if(eCodServicio.value && ePiezas.value)
             {
-                agregarFilaSupervisor(nIndice);    
+                dMonto.value = parseInt(ePiezas.value)*parseInt(dImporte.value);
+                agregarFilaPaqueteCotizacion(nIndice);    
             }
     }
     
-function agregarFilaSupervisor(indice)
+function agregarFilaPaqueteCotizacion(indice)
     {
-        var x = document.getElementById("supervisores").rows.length;
+        var x = document.getElementById("paquetes").rows.length;
         
         
-        var eCodSupervisor = document.getElementById('eCodSupervisor'+indice);
-        if(eCodSupervisor)
-            {}
-        else
-        {
-            
-            var tablapromotor = '';
-            
-            tablapromotor = '<table id="promotores" width="100%"><tr id="pro0"><td><i class="far fa-trash-alt" onclick="deleteRow(\'pro'+indice+'\',\'promotores\')"></i></td><td><input type="hidden" id="eCodPromotor'+indice+'-0" name="promotores['+indice+'][0][eCodPromotor]"><input type="text" class="form-control" id="tPromotor'+indice+'-0" name="promotores['+indice+'][0][tPromotor]" onkeyup="agregarPromotor('+indice+',\'0\')" onkeypress="agregarPromotor('+indice+',\'0\')" onblur="validarPromotor('+indice+',\'0\')"></td></tr></table>';
-           
-    var table = document.getElementById("supervisores");
-    var row = table.insertRow(x);
-    row.id="sup"+(indice);
-    row.innerHTML = '<td><table><tr><td><i class="far fa-trash-alt" onclick="deleteRow(\'sup'+indice+'\',\'supervisores\')"></i></td>';
-    row.innerHTML += '<td><input type="hidden" id="eCodSupervisor'+indice+'" name="supervisores['+indice+'][eCodSupervisor]"><input type="text" class="form-control" id="tSupervisor'+indice+'" name="supervisores['+indice+'][tSupervisor]" onkeyup="agregarSupervisor('+indice+')" onkeypress="agregarSupervisor('+indice+')" onblur="validarSupervisor('+indice+')"></td><td><input type="hidden" id="eCodTienda'+indice+'" name="tiendas['+indice+'][eCodTienda]"><input type="text" class="form-control" id="tTienda'+indice+'" name="tiendas['+indice+'][tTienda]" onkeyup="agregarTienda('+indice+')" onkeypress="agregarTienda('+indice+')"></td></tr><tr><td></td><td>'+tablaPromotor+'</td><td></td></tr></table></td>';
-        }
-        
-    }
-
-//Promotor
-
-function validarPromotor(indice,fila)
-    {
-        var eCodPromotor    =   document.getElementById('eCodPromotor'+indice+'-'+fila),
-            nIndice         =   parseInt(fila)+1;
-        
-        if(eCodPromotor.value)
-            {
-                agregarFilaPromotor(indice,nIndice);    
-            }
-    }
-    
-function agregarFilaPromotor(indice,fila)
-    {
-        var x = document.getElementById("promotores").rows.length;
-        
-        
-        var eCodPromotor = document.getElementById('eCodPromotor'+indice+'-'+fila);
-        if(eCodPromotor)
+        var tExtra = document.getElementById('paquete'+indice+'-eCodServicio');
+        if(tExtra)
             {}
         else
         {
            
-    var table = document.getElementById("promotores");
+    var table = document.getElementById("paquetes");
     var row = table.insertRow(x);
-    row.id="pro"+(indice)+'-'+(fila);
-    row.innerHTML = '<td><i class="far fa-trash-alt" onclick="deleteRow(\'pro'+indice+'\',\'promotores\')"></i></td>';
-    row.innerHTML += '<td><input type="hidden" id="eCodPromotor'+indice+'-'+fila+'" name="promotores['+indice+']['+fila+'][eCodPromotor]"><input type="text" class="form-control" id="tPromotor'+indice+'-'+fila+'" name="promotores['+indice+']['+fila+'][tPromotor]" onkeyup="agregarPromotor('+indice+','+fila+')" onkeypress="agregarPromotor('+indice+','+fila+')" onblur="validarPromotor('+indice+','+fila+')"></td>';
+    row.id="paq"+(indice);
+    row.innerHTML = '<td><i class="far fa-trash-alt" onclick="deleteRow(\'paq'+indice+'\',\'paquetes\')"></i></td>';
+    row.innerHTML += '<td><input type="checkbox" id="paquete'+indice+'-bSuma" name="paquete['+indice+'][bSuma]" value="1" onclick="calcular();"><i class="fas fa-info-circle" onclick="asignarPaquete('+indice+')"></i></td><td><input type="hidden" id="paquete'+indice+'-eCodServicio" name="paquete['+indice+'][eCodServicio]"><input type="text" class="form-control" id="tPaquete'+indice+'" name="tPaquete'+indice+'" onkeyup="agregarPaquete('+indice+')" onkeypress="agregarPaquete('+indice+')" onblur="validarPaquete('+indice+')"></td>';
+    row.innerHTML += '<td><input type="text" class="form-control" id="paquete'+indice+'-ePiezas" name="paquete['+indice+'][ePiezas]" onkeyup="validarPiezas(\'paquete'+indice+'\'); validarpaquete('+indice+')" onblur="validarPaquete('+indice+')"></td>';
+    row.innerHTML += '<td><input type="hidden" id="paquete'+indice+'-eMaxPiezas"><input type="hidden" id="paquete'+indice+'-dHoraExtra"><input type="hidden" id="paquete'+indice+'-dImporte" name="paquete['+indice+'][dImporte]"><input type="text" class="form-control" id="paquete'+indice+'-eHorasExtra" name="paquete['+indice+'][eHorasExtra]" onkeyup="calcularTotalPaquete('+indice+'); calcular();" onblur="calcularTotalPaquete('+indice+'); calcular();"></td><td><input type="text" id="paquete'+indice+'-dMonto" name="paquete['+indice+'][dMonto]" readonly></td>';
         }
         
-    }
-
-//Cliente
-
-function validarCliente(indice)
-    {
-        var eCodCliente    =   document.getElementById('eCodCliente'+indice),
-            nIndice         =   parseInt(indice)+1;
-        
-        if(eCodCliente.value)
-            {
-                agregarFilaCliente(nIndice);    
-            }
-    }
-    
-function agregarFilaCliente(indice)
-    {
-        var x = document.getElementById("clientes").rows.length;
-        
-        
-        var eCodCliente = document.getElementById('eCodCliente'+indice);
-        if(eCodCliente)
-            {}
-        else
-        {
-           
-    var table = document.getElementById("clientes");
-    var row = table.insertRow(x);
-    row.id="cli"+(indice);
-    row.innerHTML = '<td><i class="far fa-trash-alt" onclick="deleteRow(\'cli'+indice+'\',\'clientes\')"></i></td>';
-    row.innerHTML += '<td><input type="hidden" id="eCodCliente'+indice+'" name="clientes['+indice+'][eCodCliente]"><input type="text" class="form-control" id="tCliente'+indice+'" name="clientes['+indice+'][tCliente]" onkeyup="agregarCliente('+indice+')" onkeypress="agregarCliente('+indice+')" onblur="validarCliente('+indice+')"></td>';
-        }
-        
-    }
-
-//Producto
-
-function validarProducto(indice)
-    {
-        var eCodProducto    =   document.getElementById('eCodProducto'+indice),
-            tProducto    =   document.getElementById('tProducto'+indice),
-            nIndice         =   parseInt(indice)+1;
-        
-        if(eCodProducto.value || tProducto.value)
-            {
-                agregarFilaProducto(nIndice);    
-            }
-    }
-    
-function agregarFilaProducto(indice)
-    {
-        var x = document.getElementById("productos").rows.length;
-        
-        
-        var eCodProducto = document.getElementById('eCodProducto'+indice);
-        if(eCodProducto)
-            {}
-        else
-        {
-           
-    var table = document.getElementById("productos");
-    var row = table.insertRow(x);
-    row.id="prd"+(indice);
-    row.innerHTML = '<td><i class="far fa-trash-alt" onclick="deleteRow(\'prd'+indice+'\',\'productos\')"></i></td>';
-    row.innerHTML += '<td><input type="hidden" id="eCodProducto'+indice+'" name="productos['+indice+'][eCodProducto]"><input type="text" class="form-control" id="tProducto'+indice+'" name="productos['+indice+'][tProducto]" onkeyup="agregarProducto('+indice+')" onkeypress="agregarProducto('+indice+')" onblur="validarProducto('+indice+')"><br><table id="presentaciones'+indice+'"><tr id="pre'+indice+'-0"><td><i class="far fa-trash-alt" onclick="deleteRow(\'pre'+indice+'-0\',\'presentaciones'+indice+'\')"></i></td><td><input type="hidden" id="eCodPresentacion'+indice+'-0" name="presentaciones['+indice+'][eCodPresentacion]"><input type="text" class="form-control" id="tPresentacion'+indice+'-0" name="presentaciones['+indice+'][tPresentacion]" onkeyup="agregarPresentacion('+indice+',\'0\')" onkeypress="agregarPresentacion('+indice+',\'0\')" onblur="validarPresentacion('+indice+',\'0\')"></td></tr></table></td>';
-        }
+    calcular();
         
     }
